c6be157f2b0768da71eb539ff508262d618b412d
naelengithub <anaelisa@mailmasker.com>
2023-04-10 15:33:13 -0600

    chrome: added colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,6 +7,8 @@ module.exports = {
         alabaster: "#F0EDE3",
         honeydew: "#9DAE98",
         moonstone: "#75B4C1",
+        lightCoral: "#D98982",
+        cambridgeBlue: "#9DAE98",
       },
     },
   },
