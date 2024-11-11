1b81891f3d00a35d78f6306afe110f3c0c4a047f
naelengithub <anaelisa@mailmasker.com>
2024-11-11 16:16:27 +0100

    chore: updated temp landing page wording

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,7 +3,12 @@ export default function Home() {
     <div className="font-[family-name:var(--font-questrial-sans)] h-screen p-8">
       <main className="flex flex-col justify-center items-center h-4/5 text-center">
         <h1 className="text-9xl">eina</h1>
-        <h2>Compartiendo recursos para reconstruirnos en comunidad.</h2>
+        <h2>
+          Compartiendo recursos de deconstrucción
+          <br />
+          para reconstruirnos en comunidad.
+        </h2>
+        <p>Próximamente...</p>
       </main>
       <footer className=""></footer>
     </div>
