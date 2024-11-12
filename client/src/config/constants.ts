0c74ca4d6ebad3905be8c759f2b5c2d113ed1335
naelengithub <anaelisa@mailmasker.com>
2024-11-12 10:01:30 +0100

    chore: added constants

---
+++ client/src/config/constants.ts
@@ -0,0 +1,7 @@
+export const DATE_TODAY = new Date();
+
+export const SITE_HEADER = `Your Guide to DEI: Learn, Unlearn, and Celebrate Diversity`;
+export const SITE_DESCRIPTION = `An online encyclopedia of DEI-focused events, books, podcasts, and movies. Explore resources that promote learning, challenge limiting social norms, and celebrate identity, love, and inclusion.`;
+export const SITE_EMAIL_ADDRESS = `hola@eina.info`;
+export const SITE_EMAIL_LINK = `mailto:${SITE_EMAIL_ADDRESS}`;
+export const SITE_TITLE = "eina";
